744ef6cd5adbbf2fc7f71190a9f365e0db340a02
sbx_user1051 <sbx_user1051@169.254.8.109>
2020-02-20 15:07:32 +0000

    Thu Feb 20 15:07:32 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,10 +10,31 @@ public class CheckoutSolution {
         int d = 0;
         int suma = 0;
         for(int i = 0; i < skus.length(); i++){
-            if(skus[i] == "A"){
-                
+            if(skus.charAt(i) == 'A'){
+                a++;
+            } else if(skus.charAt(i) == 'B'){
+                b++;
+            } else if(skus.charAt(i) == 'C'){
+                c++;
+            } else if(skus.charAt(i) == 'D'){
+                d++;
             }
         }
+        if(a >= 3){
+            suma += (a/3)*130 + (a%3)*50;
+        } else {
+            suma += a*50;
+        }
+        if(b >= 2){
+            suma += (b/2)*45 + (b%2)*30;
+        } else {
+            suma += b*30;
+        }
+        suma += c*20;
+        suma += d*15;
+
+        return suma;
     }
 }
+
 
