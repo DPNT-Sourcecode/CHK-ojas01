496cc862ee841f51642079da2c415bba749a1bdb
sbx_user1051 <sbx_user1051@169.254.8.109>
2020-02-20 15:22:04 +0000

    Thu Feb 20 15:22:04 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,6 +8,7 @@ public class CheckoutSolution {
         int b = 0;
         int c = 0;
         int d = 0;
+        int e = 0;
         int suma = 0;
         for(int i = 0; i < skus.length(); i++){
             if(skus.charAt(i) == 'A'){
@@ -18,11 +19,22 @@ public class CheckoutSolution {
                 c++;
             } else if(skus.charAt(i) == 'D'){
                 d++;
+            } else if(skus.charAt(i) == 'E') {
+                e++;
             } else {
                 return -1;
             }
         }
-        if(a >= 3){
+        if(a >= 5){
+            int rest;
+            rest = a%5;
+            if(rest >= 3){
+                suma += (a/5)*200 + (rest/3)*130 + (rest%3)*50;
+            } else {
+                suma += (a/5)*200 + rest * 50;
+            }
+        }
+        if(a >= 3 && a < 5){
             suma += (a/3)*130 + (a%3)*50;
         } else {
             suma += a*50;
@@ -32,9 +44,15 @@ public class CheckoutSolution {
         } else {
             suma += b*30;
         }
+        if(e >= 2){
+            suma += e*40 - (e/2)*30;
+        } else {
+            suma += e*40;
+        }
         suma += c*20;
         suma += d*15;
 
         return suma;
     }
 }
+
